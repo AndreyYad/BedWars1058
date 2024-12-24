f21a22203064bd2d63e9360b045d14143456cc38
Salten <sea.salten@gmail.com>
2024-12-24 08:19:14 +0300

    local

---
+++ bedwars-plugin/src/main/java/com/andrei1058/bedwars/items/BWItem.java
@@ -0,0 +1,4 @@
+package com.andrei1058.bedwars.items;
+
+public interface BWItem {
+}
---
+++ bedwars-plugin/src/main/java/com/andrei1058/bedwars/items/control/ItemController.java
@@ -0,0 +1,11 @@
+package com.andrei1058.bedwars.items.control;
+
+import com.andrei1058.bedwars._fwextension.common.templates.ITemplate;
+import com.andrei1058.bedwars.items.BWItem;
+
+public class ItemController {
+
+    static public BWItem createItem(ITemplate itemTemplate) {
+        return null;
+    }
+}
